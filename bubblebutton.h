71d596e192c0d8945cbc48709ffb22c3a696cfee
YashGovindani <yashgovindani222@gmail.com>
2021-08-08 01:30:09 +0530

    defined constructor

--- bubblebutton.h
+++ bubblebutton.h
@@ -2,12 +2,14 @@
 #define BUBBLEBUTTON_H
 
 #include <QPushButton>
+#include <QGraphicsDropShadowEffect>
 
 class BubbleButton:public QPushButton
 {
 private:
     BubbleButton(QWidget *loadingView = nullptr);
     static BubbleButton *bubbleButton;
+    QGraphicsDropShadowEffect *shadow_effect;
 public:
     static BubbleButton * get(QWidget *loadingView = nullptr);
 };
