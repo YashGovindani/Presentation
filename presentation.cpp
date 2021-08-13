ae76ccb2af9211257a8703862bc549a214e35f10
YashGovindani <yashgovindani222@gmail.com>
2021-08-13 17:23:40 +0530

    initial settings of ui

--- presentation.cpp
+++ presentation.cpp
@@ -4,6 +4,11 @@
 #include "loadingview.h"
 #include "initialview.h"
 
+#include <QScreen>
+#include <QGuiApplication>
+#include <QRect>
+#include <QFont>
+
 Presentation *Presentation::presentation = nullptr;
 bool Presentation::toolFrameInitiated = false;
 
@@ -13,7 +18,17 @@ Presentation::Presentation(QWidget *loadingView, QWidget *parent)
 {
     ui->setupUi(this);
     this->setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
-    this->setAttribute(Qt::WA_TranslucentBackground);
+    this->setGeometry(QGuiApplication::primaryScreen()->geometry());
+    this->setStyleSheet(QString("background-color : rgb(46, 47, 48)"));
+    QFont font;
+    font.setPointSize(64);
+    font.setBold(true);
+    this->ui->mainViewLabel->setGeometry(this->geometry());
+    this->ui->mainViewLabel->setText(QString("Welcome"));
+    this->ui->mainViewLabel->setFont(font);
+    this->ui->mainViewLabel->setAlignment(Qt::AlignCenter);
+    this->ui->mainViewLabel->setStyleSheet(QString("color : rgba(1,1,1,25); background-color : rgba(0,0,0,0)"));
+    this->ui->toolFrame->setStyleSheet(QString("background-color : rgba(255, 255, 255, 25); border : 1px solid white;"));
 }
 
 void Presentation::setToolFramePosition(QWidget *bubbleButton)
